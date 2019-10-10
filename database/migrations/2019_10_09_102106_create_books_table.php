43485b8085f9c3d454efc4bff82aceae25050687
HiroshiMorita <ikimonogakaru@gmail.com>
2019-10-10 16:29:28 +0900

    データベース初期設定

--- database/migrations/2019_10_09_102106_create_books_table.php
+++ database/migrations/2019_10_09_102106_create_books_table.php
@@ -14,7 +14,8 @@ class CreateBooksTable extends Migration
     public function up()
     {
         Schema::create('books', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->incrementsIncrements('id');
+            $table->string('title');
             $table->timestamps();
         });
     }
